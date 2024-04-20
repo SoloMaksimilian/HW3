acec015d9d4a9069574c3793b7b587a60f8ec6b7
SoloMaksimilian <mephistosmad@gmail.com>
2024-04-20 19:30:33 +0300

    Ex 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = -28;
+        short b = 31000;
+        int c = -456789;
+        long d = 1234567890L;
+        float e = 123.456f;
+        double f = 98.76543201;
+            System.out.println("Значение переменной a с типом byte равно " + a);
+            System.out.println("Значение переменной b с типом short равно " + b);
+            System.out.println("Значение переменной c с типом int равно " + c);
+            System.out.println("Значение переменной d с типом long равно " + d);
+            System.out.println("Значение переменной e с типом float равно " + e);
+            System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
